30ca30174ea95cc28ece66db621bce984ce3603e
Naeem <jannatunnaeem141@gmail.com>
2026-04-04 12:29:31 +0600

    Update favicon and add metadata

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -4,8 +4,40 @@ import { ThemeProvider } from "@/components/theme/ThemeProvider";
 import { Toaster } from "@/components/ui/sonner";
 
 export const metadata: Metadata = {
-  title: "Diceymio - Board Game Store",
+  title: "Diceymio - Board Game Universe",
   description: "Buy your favorite board games online",
+  keywords: ["Board Games"],
+  authors: [{ name: "Diceymio" }],
+  icons: {
+    icon: "/favicon.ico",
+  },
+  openGraph: {
+    type: "website",
+    url: "https://diceymio.com",
+    title: "Diceymio",
+    description: "Buy your favorite board games online",
+    siteName: "Diceymio",
+    images: [
+      {
+        url: "",
+        alt: "Diceymio - Board Game Universe",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Diceymio - Board Game Universe",
+    description: "Buy your favorite board games online",
+    images: [],
+  },
+  // robots: {
+  //   index: true,
+  //   follow: true,
+  //   googleBot: {
+  //     index: true,
+  //     follow: true,
+  //   },
+  // },
 };
 
 export default function RootLayout({
@@ -17,8 +49,15 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
+          rel="stylesheet"
+        />
       </head>
       <body>
         <ThemeProvider
